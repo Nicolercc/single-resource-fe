a07a08350fe61b13648ecb74a7d60dacc824b33f
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2023-11-06 19:41:02 -0500

    added dependencies started navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/colors" element={<Index />} /> */}
+            {/* <Route path="/colors/new" element={<New />} /> */}
+            {/* <Route path="/colors/:index" element={<Show />} /> */}
+            {/* <Route path="/colors/:index/edit" element={<Edit />} /> */}
+            {/* <Route path="*" element={<FourOFour />} /> */}
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
